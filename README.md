941a9b623447b49291a006ce1f238a344a216352
harshalkore <harshalkore10@gmail.com>
2025-05-03 00:21:55 +0530

    Import complete codebase

---
+++ README.md
@@ -0,0 +1,12 @@
+# Task List Applicaiton
+
+• Built a front-end task list application using HTML, CSS and JavaScript.
+• Utilized JavaScript to dynamically update the DOM based on user interactions.
+• Implemented event handling for task management (add, complete, delete).
+• Implemented local storage to save task data between sessions.
+• Included a user-friendly light/dark mode toggle.
+
+Screenshots of the Web Page are attached below:
+
+![Web Page Screenshot](img/task_list1.png)
+![Web Page Screenshot](img/task_list2.png)
---
+++ script.js
@@ -0,0 +1,45 @@
+let modeBtn = document.querySelector("#mode");
+let currmode = "light";
+modeBtn.addEventListener("click",()=>{
+    document.querySelector(".container").classList.toggle("dark");
+})
+
+const inputbox = document.getElementById("input_box");
+const listcontainer = document.getElementById("list_container");
+
+function add_task(){
+    if(inputbox.value === ''){
+        alert("You must add something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span =document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = '';
+    saveData();
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML)
+}
+
+function showTask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
